b63c9243ba7ff26c9ec87eab99c4736bc02fbb01
dansta <danne@kvantmekanik.se>
2017-12-20 21:23:39 +0100

    Commit correct replace.py

--- postfix/replace.py
+++ postfix/replace.py
@@ -3,29 +3,49 @@
 
 import os
 import sys
+import re
+
 
 # read call parameters
 FD = sys.argv[1]
 KEYWORD = sys.argv[2]
-ENV = {}
-
-
-# open the config for reading
+REPLACEMENTDICT = {}
+# open the config for reading, we replace in memory
 try:
-    CONFIGFILE = open(FD)
+    with open(FD, 'r') as CONFIGFILE:
+        CONFIGDATA = CONFIGFILE.read()
+        CONFIGFILE.close()
+# strip comments because they are not needed in containers
+        CONFIGDATA = re.sub(r'#.*\n',
+                            r'',
+                            CONFIGDATA)
 except IOError:
     exit(1)
 
 # read the environment variables that match
-for i in sorted(os.environ):
-    if KEYWORD in i:
-        y = os.environ[i]
-        ENV(i:y)
-
-# find and replace in the config file
-for i in CONFIGFILE:
-    if KEYWORD in i:
-        line = i
+for ENV_VAR in sorted(os.environ):
+    if KEYWORD in ENV_VAR:
+        KEY = re.sub(r'=.*\n',
+                     r'',
+                     ENV_VAR)
+        VALUE = os.environ[ENV_VAR]
+        REPLACEMENTDICT[KEY] = VALUE
 
 
-print(ENV)
+# find and substitute env variables based on keyword prefix in the config file
+for KEY, VALUE in REPLACEMENTDICT.items():
+#    print(KEY, VALUE)
+    if KEY in CONFIGDATA:
+        CONFIGDATA = re.sub(KEY,
+                            VALUE,
+                            CONFIGDATA)
+
+# open the config again for writing
+try:
+    with open(FD, 'w') as CONFIGFILE:
+        CONFIGFILE.write(CONFIGDATA)
+except IOError:
+    exit(1)
+
+# close politely
+CONFIGFILE.close()
