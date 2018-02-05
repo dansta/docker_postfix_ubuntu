d46e4191b48cb734cba3516d35a8138ebb2dd9ad
Daniel Ståhl <daniel.stahl@pensionsmyndigheten.se>
2018-02-05 12:19:14 +0100

    Sudoers is not installed and is not needed

--- postfix/debconf.sh
+++ postfix/debconf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo debconf debconf/frontend select noninteractive | sudo debconf-set-selections
-echo postfix postfix/mailname string ${POSTFIX_FQDN} | sudo debconf-set-selections
-echo postfix postfix/main_mailer_type select Internet Site | sudo debconf-set-selections
+echo debconf debconf/frontend select noninteractive | debconf-set-selections
+echo postfix postfix/mailname string ${POSTFIX_FQDN} | debconf-set-selections
+echo postfix postfix/main_mailer_type select Internet Site | debconf-set-selections
